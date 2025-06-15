031b29d03fff93bf02885fae01e362b42411dce0
Robert <robert.pastor0691@orange.fr>
2025-06-15 22:59:23 +0200

    loop through aircraft types

--- trajectory/management/commands/TrajectoryComputeWrap.py
+++ trajectory/management/commands/TrajectoryComputeWrap.py
@@ -8,6 +8,11 @@ from django.core.management.base import BaseCommand
 from airline.models import AirlineRoute
 
 from trajectory.GuidanceOpenap.FlightPathOpenapFile import FlightPathOpenap
+from trajectory.Openap.AircraftMainFile import OpenapAircraft
+
+from trajectory.Environment.Earth import Earth
+from trajectory.Environment.Atmosphere import Atmosphere
+
 from openap import prop
 
 
@@ -19,6 +24,9 @@ class Command(BaseCommand):
         logging.basicConfig(level=logging.INFO)
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
+        
+        earth = Earth()
+        atmosphere = Atmosphere()
         
         start_time = time()
         
@@ -36,54 +44,71 @@ class Command(BaseCommand):
         AdesRunway = "26L"
         #AdesRunway = "16L"
         
-        takeOffWeightKg = 64000.0
-        #takeOffWeightKg = 230000.0
-        
-        if not ( aircraftICAOcode in prop.available_aircraft(use_synonym=True) ):
-            print ( "Aircraft code = {0} not in openap Wrap".format( aircraftICAOcode ))
-        else:
+        available_acs = prop.available_aircraft(use_synonym=True)
+
+        for aircraftICAOcode in available_acs:
             
-            Adep = str(route).split("-")[0]
-            Ades = str(route).split("-")[1]
-            airlineRoute = AirlineRoute.objects.filter(DepartureAirportICAOCode=Adep, ArrivalAirportICAOCode=Ades).first()
-            if ( airlineRoute ):
-                ''' try with direct route '''
-                routeAsString = airlineRoute.getRouteAsString( AdepRunWayName = AdepRunway, AdesRunWayName = AdesRunway , direct = False)
-                logging.info ( "Trajectory Compute Wrap - " + routeAsString )
-                
-                flightPath = FlightPathOpenap(
-                        route                = routeAsString, 
-                        aircraftICAOcode     = aircraftICAOcode,
-                        RequestedFlightLevel = 390.0, 
-                        cruiseMach           = 0.82, 
-                        takeOffMassKilograms = takeOffWeightKg)
-                try:
-                    flightPath.computeFlight(deltaTimeSeconds = 1.0)
+            if ( str( aircraftICAOcode ).lower() in ['a359','a388','b38m','b744','b748','b752','b763','b773','b77w','b788','b789','c550'] \
+                 or str( aircraftICAOcode ).lower() in ['e145','glf6','a124','a306','a310','at72','at75','at76','b733','b735','b762','b77l'] \
+                 or str ( aircraftICAOcode ).lower() in ['c25a','c525','c56x','crj2','crj9','e290','glf5','gl5t','gl6t','tj45','md11','pc24','su95','lj45','bx3m'] ):
+                #pass
+                continue
+        
+            ac = OpenapAircraft( aircraftICAOcode , earth , atmosphere , initialMassKilograms = None)
+            logging.info( ac.getAircraftName())
+
+            takeOffWeightKg = ac.getReferenceMassKilograms()
+            logging.info("take off weight = {0:.2f} kg".format( takeOffWeightKg ))
+            cruiseFlightLevel = ac.getMaxCruiseAltitudeFeet() / 100.0
+            logging.info("cruise level FL = {0:.2f} ".format ( cruiseFlightLevel ) )
+            
+            targetCruiseMach = ac.getMaximumSpeedMmoMach()
+            logging.info( "target cruise mach = {0:.2f} ".format( targetCruiseMach ) )
+            
+            if not ( aircraftICAOcode in prop.available_aircraft(use_synonym=True) ):
+                print ( "Aircraft code = {0} not in openap Wrap".format( aircraftICAOcode ))
+            else:
+            
+                Adep = str(route).split("-")[0]
+                Ades = str(route).split("-")[1]
+                airlineRoute = AirlineRoute.objects.filter(DepartureAirportICAOCode=Adep, ArrivalAirportICAOCode=Ades).first()
+                if ( airlineRoute ):
+                    ''' try with direct route '''
+                    routeAsString = airlineRoute.getRouteAsString( AdepRunWayName = AdepRunway, AdesRunWayName = AdesRunway , direct = False)
+                    logging.info ( "Trajectory Compute Wrap - " + routeAsString )
+                    
+                    flightPath = FlightPathOpenap(
+                            route                = routeAsString, 
+                            aircraftICAOcode     = aircraftICAOcode,
+                            RequestedFlightLevel = cruiseFlightLevel, 
+                            cruiseMach           = targetCruiseMach, 
+                            takeOffMassKilograms = takeOffWeightKg)
+                    try:
+                        flightPath.computeFlight(deltaTimeSeconds = 1.0)
+                        
+                        end_time = time()
+                        seconds_elapsed = end_time - start_time
+                    
+                        hours, rest = divmod(seconds_elapsed, 3600)
+                        minutes, seconds = divmod(rest, 60)
+                        print ( "hours = {0} - minutes = {1} - seconds = {2:.2f}".format( hours, minutes, seconds))
+                    
+                        flightPath.createStateVectorHistoryFile()
+                        flightPath.createKmlXmlDocument()
+                    
+                    except Exception as e:
+                        logging.error("Trajectory Compute Wrap - Exception = {0}".format( str(e ) ) )
+
+                    #print ( "Trajectory Compute - distance flown = {0:.2f} meters - {1:.2f} Nm".format( flightPath.flightLengthMeters , flightPath.flightLengthMeters * Meter2NauticalMiles ))
                     
                     end_time = time()
                     seconds_elapsed = end_time - start_time
-                
+                    
                     hours, rest = divmod(seconds_elapsed, 3600)
                     minutes, seconds = divmod(rest, 60)
-                    print ( "hours = {0} - minutes = {1} - seconds = {2:.2f}".format( hours, minutes, seconds))
-                
-                    flightPath.createStateVectorHistoryFile()
-                    flightPath.createKmlXmlDocument()
-
-                except Exception as e:
-                    logging.error("Trajectory Compute Wrap - Exception = {0}".format( str(e ) ) )
-
-                
-                #print ( "Trajectory Compute - distance flown = {0:.2f} meters - {1:.2f} Nm".format( flightPath.flightLengthMeters , flightPath.flightLengthMeters * Meter2NauticalMiles ))
-                
-                end_time = time()
-                seconds_elapsed = end_time - start_time
-                
-                hours, rest = divmod(seconds_elapsed, 3600)
-                minutes, seconds = divmod(rest, 60)
-                print ( "hours = {0} - minutes = {1} - seconds = {2}".format( hours, minutes, seconds))
+                    print ( "hours = {0} - minutes = {1} - seconds = {2}".format( hours, minutes, seconds))
                     
-            else:
-                print ('airline route not found = {0}'.format(route))
+                else:
+                    print ('airline route not found = {0}'.format(route))
             
             
